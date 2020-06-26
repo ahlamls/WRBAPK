772a48eb747bd278cf012cfa3298249be81a8763
ahlamls <fulldoang@gmail.com>
2020-06-26 21:30:01 +0700

    Initial commit

---
+++ app/src/main/java/id/wrbcatering/aplikasi/SplashActivity.java
@@ -0,0 +1,14 @@
+package id.wrbcatering.aplikasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
